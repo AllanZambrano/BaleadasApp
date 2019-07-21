4237c4bcaa6e1b2e0ffc15d5f6364f42fcff072f
Allan Zambrano <allazambrano@gmail.com>
2019-07-21 15:16:53 -0300

    Heroku push

--- Baleadas/settings.py
+++ Baleadas/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,6 @@ LEAFLET_CONFIG = {
     'MAX_ZOOM': 18,
 
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
