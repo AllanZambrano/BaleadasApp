b9ea2d40b44317fdd22e6f6cf4e1565d48bc1340
Allan Zambrano <allazambrano@gmail.com>
2019-07-25 22:53:03 -0300

    Update: Template, requirements.txt, settings

--- markers/views.py
+++ markers/views.py
@@ -3,13 +3,15 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from .models import Marker
 from .forms import MarkerForm
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class Map(ListView):
     model = Marker
     template_name = 'map.html'
 
-class CreateEntry(CreateView):
+class CreateEntry(SuccessMessageMixin,CreateView):
     form_class = MarkerForm
     template_name = 'add.html'
+    success_message = "Marker was created successfully"
     success_url = reverse_lazy('map')
